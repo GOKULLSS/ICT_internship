a1bee139220b84922ce3fa2ad7922c5d2378d325
Gokul <gokulofficial556@gmail.com>
2026-06-05 09:38:26 +0530

    backend initialised

--- StudentProject/backend/app.js
+++ StudentProject/backend/app.js
@@ -1,7 +1,18 @@
 const express =require('express');
 const app=express();
 const port=3000;
+const userRoutes = require('./routes/userRoutes');
+const userModel=require('./models/userModel');
+const db=require('./db');
+const connectDB = require('./db');
+require('dotenv').config();
+
+app.use(express.json());
+connectDB();
+app.use('/',userRoutes);
+
 
 app.listen(port,()=>{
     console.log("connected to port:",port)
 })
+
